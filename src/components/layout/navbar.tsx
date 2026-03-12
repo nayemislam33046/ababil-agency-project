f5b02189a918f45c55e26933a3f23cdfc4869bf1
Nayem <nayemtruthness@gmail.com>
2026-03-12 19:27:24 +0600

    Ababil

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -8,20 +8,22 @@ const Navbar = () => {
 
       <Link href="#" className="hover:text-yellow-400 text-sm sm:text-body-base font-medium flex justify-center items-center flex-col gap-2">
         <span className='sm:hidden'>
-          <Briefcase size={20} />
+          <Image src="/projects_icon.svg" alt="projects_icon" width={24} height={24} />
         </span>
         Projects
       </Link>
 
       <Link href="#" className="hover:text-yellow-400 text-sm sm:text-body-base font-medium flex justify-center items-center flex-col gap-2">
         <span className='sm:hidden'>
-          <Puzzle size={20} />
+          <Image src="/services_icon.svg" alt="services_icon" width={24} height={24} />
         </span>
         Services
       </Link>
 
-      <Link href="#" className="bg-primary border-8 sm:border-none border-white sm:bg-white text-[#0A3D2E] px-6 py-2 rounded-[30px] sm:rounded-lg font-bold flex items-center gap-2 -translate-y-12 sm:translate-y-0 sm:static w-max h-24 sm:w-auto sm:h-auto">
+      <Link href="#" className="bg-primary border-8 sm:border-none border-white sm:bg-white text-[#0A3D2E] px-4 lm:px-6 py-2 rounded-[30px] sm:rounded-lg font-bold flex items-center gap-2 -translate-y-8 lm:-translate-y-10 sm:translate-y-0 sm:static h-22 lm:h-24 w-max sm:w-auto sm:h-auto">
 
+        <span className="w-3.5 h-3.5 bg-transparent absolute -left-5.5 top-6 lm:top-8 rounded-tr-[11px] sm:hidden myShadow1"></span>
+        <span className="w-3.5 h-3.5 bg-transparent absolute -right-5.5 top-6 lm:top-8 rounded-tl-[11px] sm:hidden myShadow2"></span>
         <span>
           <Image
             src="/logoIcon.png"
@@ -46,14 +48,14 @@ const Navbar = () => {
 
       <Link href="#" className="hover:text-yellow-400 text-sm sm:text-body-base font-medium flex justify-center items-center flex-col gap-2">
         <span className='sm:hidden'>
-          <DollarSign size={20} />
+          <Image src="/doller_icon.svg" alt="doller_icon" width={24} height={24} />
         </span>
         Pricing
       </Link>
 
       <Link href="#" className="hover:text-yellow-400 text-sm sm:text-body-base font-medium flex justify-center items-center flex-col gap-2">
         <span className='sm:hidden'>
-          <Menu size={20} />
+          <Image src="/menu_icon.svg" alt="menu_icon" width={24} height={24} />
         </span>
         More
       </Link>
